06e64c73929a071a72b36a257b2b99b48788c9fc
Aleksander Kostopoulos <aleksander.kostopoulos@gmail.com>
2025-09-07 15:22:55 +0200

    endret Header til header for deploying til netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./components/footer/Footer.jsx";
-import Header from "./components/Header/Header.jsx";
+import Header from "./components/header/Header.jsx";
 import Products from "./components/products/Products.jsx";
 import Modal from "./components/modal/Modal.jsx";
 import Hero from "./components/hero/Hero.jsx";
